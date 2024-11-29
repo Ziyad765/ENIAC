bd02358b05c5c7c29a19418d2ee443438d3934a6
Ziyad Ahammed <ziyadkomakkal@gmail.com>
2024-11-30 03:08:24 +0530

    Delete info.py

--- info.py
+++
@@ -1,9 +0,0 @@
-import g4f
-
-# Print available providers and models
-print("Available Providers:")
-print(dir(g4f.Provider))
-
-print("\nAvailable Models:")
-# This part depends on how models are handled in g4f; adjust if needed
-print(dir(g4f.ChatCompletion))
